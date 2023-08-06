ef9a74a7d0b929ab35675d565f6ab473b9b3764c
Artem Voytenko <av.voytenko@gmail.com>
2023-08-06 20:14:17 +0300

    Use docker-compose

--- metric/src/metric.py
+++ metric/src/metric.py
@@ -1,52 +1,62 @@
 import pika
 import json
 
-# словарь для хранения данных полученных из очередей
-data_store = {
-    'y_true': None,
-    'y_pred': None
-}
 
-# Создаём подключение к серверу на локальном хосте:
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+try:
+    # словарь для хранения данных полученных из очередей
+    data_store = {
+        'y_true': None,
+        'y_pred': None
+    }
 
-# Объявляем очереди
-channel.queue_declare(queue='y_true')
-channel.queue_declare(queue='y_pred')
+    # Создаём подключение к серверу на локальном хосте:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
 
-def process_data():
-    # Обработчик нужен для того что бы выводить результат и считать метрики,
-    # только когда получены оба значения из очередей
-    if data_store['y_true'] is not None and data_store['y_pred'] is not None:
-        print(f"y_true: {data_store['y_true']:.2f}, y_pred: {data_store['y_pred']:.2f}")
+    # Объявляем очереди
+    channel.queue_declare(queue='y_true')
+    channel.queue_declare(queue='y_pred')
+
+    def process_data():
+        # Обработчик нужен для того что бы выводить результат и считать метрики,
+        # только когда получены оба значения из очередей
+        if data_store['y_true'] is not None and data_store['y_pred'] is not None:
+            print(f"y_true: {data_store['y_true']:.2f}, y_pred: {data_store['y_pred']:.2f}")
+            
+            mape = abs((data_store['y_true'] / data_store['y_pred'] - 1) * 100)
+            print(f"MAPE: {mape:.2f} %")
+            
+            # Отчистка словаря для ожидания следующей пары значений
+            data_store['y_true'] = None
+            data_store['y_pred'] = None
+            
+    def callback(ch, method, properties, body):
+        # Обработчик данных полученных из очереди
+        # из method.routing_key получаю имя очереди, кторое является ключом для data_store
+        key, val = method.routing_key, json.loads(body)
+        data_store[key] = val
         
-        mape = abs((data_store['y_true'] / data_store['y_pred'] - 1) * 100)
-        print(f"MAPE: {mape:.2f} %")
+        answer_string = f'Из очереди {key} получено значение {val}'
+        with open('./logs/labels_log.txt', 'a') as log:
+            log.write(answer_string +'\n')
         
-        # Отчистка словаря для ожидания следующей пары значений
-        data_store['y_true'] = None
-        data_store['y_pred'] = None
-        
-def callback(ch, method, properties, body):
-    # Обработчик данных полученных из очереди
-    # из method.routing_key получаю имя очереди, кторое является ключом для data_store
-    data_store[method.routing_key] = json.loads(body)
-    process_data()
+        process_data()
 
-# Извлекаем сообщения из очередей
-channel.basic_consume(
-    queue='y_true',
-    on_message_callback=callback,
-    auto_ack=True
-)
+    # Извлекаем сообщения из очередей
+    channel.basic_consume(
+        queue='y_true',
+        on_message_callback=callback,
+        auto_ack=True
+    )
 
-channel.basic_consume(
-    queue='y_pred',
-    on_message_callback=callback,
-    auto_ack=True
-)
-print('...Ожидание сообщений, для выхода нажмите CTRL+C')
+    channel.basic_consume(
+        queue='y_pred',
+        on_message_callback=callback,
+        auto_ack=True
+    )
+    print('...Ожидание сообщений, для выхода нажмите CTRL+C')
 
-# Запускаем режим ожидания прихода сообщений
-channel.start_consuming()
+    # Запускаем режим ожидания прихода сообщений
+    channel.start_consuming()
+except:
+    print('Не удалось подключиться к очереди')
--- model/src/model.py
+++ model/src/model.py
@@ -7,33 +7,36 @@ import json
 with open('myfile.pkl', 'rb') as pkl_file:
     regressor = pickle.load(pkl_file)
 
-# Создаём подключение к серверу на локальном хосте:
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+try:
+    # Создаём подключение к серверу на локальном хосте:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
 
-# Объявляем очереди
-channel.queue_declare(queue='features')
-channel.queue_declare(queue='y_pred')
+    # Объявляем очереди
+    channel.queue_declare(queue='features')
+    channel.queue_declare(queue='y_pred')
 
-# Создаём функцию callback для обработки данных из очереди y_pred
-def callback(ch, method, properties, body):
-    json_lst = np.array(json.loads(body))
-    features = json_lst.reshape(1, -1)
-    prediction = regressor.predict(features)[0]
-    
-    # Публикуем сообщение в очередь y_pred
-    channel.basic_publish(exchange='',
-                          routing_key='y_pred',
-                          body=json.dumps(prediction))
-    print(f'Предсказание {prediction} отправлено в очередь y_pred')
+    # Создаём функцию callback для обработки данных из очереди y_pred
+    def callback(ch, method, properties, body):
+        json_lst = np.array(json.loads(body))
+        features = json_lst.reshape(1, -1)
+        prediction = regressor.predict(features)[0]
+        
+        # Публикуем сообщение в очередь y_pred
+        channel.basic_publish(exchange='',
+                            routing_key='y_pred',
+                            body=json.dumps(prediction))
+        print(f'Предсказание {prediction} отправлено в очередь y_pred')
 
-# Извлекаем сообщение из очереди features
-channel.basic_consume(
-    queue='features',
-    on_message_callback=callback,
-    auto_ack=True
-)
-print('...Ожидание сообщений, для выхода нажмите CTRL+C')
+    # Извлекаем сообщение из очереди features
+    channel.basic_consume(
+        queue='features',
+        on_message_callback=callback,
+        auto_ack=True
+    )
+    print('...Ожидание сообщений, для выхода нажмите CTRL+C')
 
-# Запускаем режим ожидания прихода сообщений
-channel.start_consuming()
+    # Запускаем режим ожидания прихода сообщений
+    channel.start_consuming()
+except:
+    print('Не удалось подключиться к очереди')
